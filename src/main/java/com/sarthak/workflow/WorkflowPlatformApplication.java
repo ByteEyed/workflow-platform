730f380672bd2e67c8bb0da037a26dc68ee2b51e
Sarthak Utekar <utekarsandeep239@gmail.com>
2026-02-27 19:13:41 +0530

    Initial commit

---
+++ src/main/java/com/sarthak/workflow/WorkflowPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.sarthak.workflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkflowPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkflowPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sarthak/workflow/WorkflowPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sarthak.workflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkflowPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
